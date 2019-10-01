2455d8fd63c128dc22f494e4b1a92ec2c79c2845
Lal Jose <tolaljose@gmail.com>
2019-10-01 13:37:39 +0200

    comments update 0.01 network_base.py

--- tracking/python/deeptam_tracker/models/networks_base.py
+++ tracking/python/deeptam_tracker/models/networks_base.py
@@ -1,10 +1,47 @@
 from abc import ABC, abstractmethod
 import tensorflow as tf
 
+"""
+networks_base.py
+----------------
+
+classes:
+    1. TrackingNetworkBase
+"""
+
+
 class TrackingNetworkBase(ABC):
+    """
+    TrackingNetworkBase
+    -------------------
+
+    parents:
+        1. ABC
+
+    members (variables):
+        1. _placeholders
+
+    members (functions):
+        1. constructor __init()___
+        2. property placeholders()
+        3. abstractmethod build_net()
+    """
 
     def __init__(self, batch_size=1):
-
+        """
+        - note:
+        - input:
+            - batch_size: int
+        - return: dictionary of placeholders
+                - 6 entries (all tf.float32):
+                    - depth_key:
+                    - image_key:image_current:
+                    - intrinsics:
+                    - prev_rotation:
+                    - prev_tanslation:
+        - functionality:
+            - create placeholders for the network
+        """
         self._placeholders = {
             'depth_key': tf.placeholder(tf.float32, shape=(batch_size, 1, 96, 128)),
             'image_key': tf.placeholder(tf.float32, shape=(batch_size, 3, 96, 128)),
@@ -16,29 +53,38 @@ class TrackingNetworkBase(ABC):
 
     @property
     def placeholders(self):
-        """All placeholders required for feeding this network"""
+        """
+        1. __init__():
+            - note:
+            - input: -
+            - return: dictionary of the placeholders
+                - All placeholders required for feeding this network
+            - functionality:
+                - to avail the placeholders of the network
+        """
         return self._placeholders
-
 
     @abstractmethod
     def build_net(self, depth_key, image_key, image_current, intrinsics, prev_rotation, prev_translation):
-        """Build the tracking network
-
-        depth_key: the depth map of the key frame
-        image_key: the image of the key frame
-        image_current: the current image
-        intrinsics: the camera intrinsics
-        prev_rotation: the current guess for the camera rotation as angle axis representation
-        prev_translation: the current guess for the camera translation
-
-        Returns all network outputs as a dict.
-        The following must be returned:
-
-            predict_rotation
-            predict_translation
-
+        """
+        2. build_net():
+            - note:
+            - input:
+                - depth_key: the depth map of the key frame
+                - image_key: the image of the key frame
+                - image_current: the current image
+                - intrinsics: the camera intrinsics
+                - prev_rotation: the current guess for the camera rotation as angle axis representation
+                - prev_translation: the current guess for the camera translation
+            - return:
+                - network outputs as a dict.
+                - The following must be returned:
+                    - predict_rotation
+                    - predict_translation
+            - functionality:
+                - Build the tracking network
         """
         pass
 
 
-
+""" End of the script """
